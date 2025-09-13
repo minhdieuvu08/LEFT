9089df3e5acbb7955c49e94c65b28e3dc641b2af
Vu Dieu Minh <vudieuminh1973@gmail.com>
2025-09-13 14:22:55 +0000

    Update executor_example.py

--- executor_example.py
+++ executor_example.py
@@ -4,13 +4,13 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 
 import torch
-from LEFT.left.generalized_fol_executor import (
+from left.generalized_fol_executor import (
     NCGeneralizedFOLExecutor,
     _get_self_mask,
     _do_apply_self_mask
 )
-from Concepts.concepts.dsl.function_domain import FunctionDomain
-from Concepts.concepts.dsl.parsers import parser_base
+from concepts.dsl.function_domain import FunctionDomain
+from concepts.dsl.parsers import parser_base
 #from Concepts.concepts.dsl.executors.function_domain_executor import FunctionDomainExecutor 
 
 def main():
